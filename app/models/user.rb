edb61ee06a0a9e8cfe6c5d50aeaa4f7ef11968d0
davemgj84 <davemgj@gmail.com>
2020-10-20 01:57:25 +0000

    create specs for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,25 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false } 
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true, length: { minimum: 8 }
+  before_save :lowercase
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def lowercase
+    self.email.downcase!
+  end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,92 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'Validations' do
+
+    it 'Validates a new user' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: "1@mail.com", password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      expect(new_user).to be_valid
+    end
+
+    it 'Does not validate when the password and the password confirmation fields do not match' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: "1@mail.com", password: "12345678", password_confirmation: "12345679")
+      new_user.save
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Requires emails to be unique (not case sensitive)' do
+      new_user1 = User.new(first_name: "David", last_name: "Jardine", email: "test@test.com", password: "12345678", password_confirmation: "12345678")
+      new_user1.save
+      new_user2 = User.new(first_name: "Matthew", last_name: "Jardine", email: "TEST@TEST.com", password: "12345678", password_confirmation: "12345678")
+      new_user2.save
+      expect(new_user2).to_not be_valid
+    end
+
+    it 'Will not validate user without a first name' do
+      new_user = User.new(first_name: nil, last_name: "Jardine", email: "1@mail.com", password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Will not validate user without a last name' do
+      new_user = User.new(first_name: "David", last_name: nil, email: "1@mail.com", password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Will not validate user without an email' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: nil, password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Will not validate user when password is less than 8 characters ' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: "1@mail.com", password: "1234567", password_confirmation: "1234567")
+      new_user.save
+      expect(new_user).to_not be_valid
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+
+    it 'Authenticates credentials with correct email and password' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: "1@mail.com", password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      authenticate = User.authenticate_with_credentials("1@mail.com", "12345678")
+      expect(authenticate).to eql(new_user)
+    end
+
+    it 'Does not authenticate with incorrect password' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: "1@mail.com", password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      authenticate = User.authenticate_with_credentials("1@mail.com", "12345")
+      expect(authenticate).to be_nil
+    end
+
+    it 'Does not authenticate with incorrect email' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: "1@mail.com", password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      authenticate = User.authenticate_with_credentials("2@mail.com", "12345678")
+      expect(authenticate).to be_nil
+    end
+
+    it 'Authenticates email ignoring the whitespace before or after' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: "1@mail.com", password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      authenticate = User.authenticate_with_credentials("    1@mail.com    ", "12345678")
+      expect(authenticate).to eql(new_user)
+    end
+
+    it 'Authenticates email - is case insensitive' do
+      new_user = User.new(first_name: "David", last_name: "Jardine", email: "eXample@domain.COM", password: "12345678", password_confirmation: "12345678")
+      new_user.save
+      authenticate = User.authenticate_with_credentials("EXAMPLe@DOMAIN.CoM", "12345678")
+      expect(authenticate).to eql(new_user)
+    end
+
+  end
+
+end
