5bc0c99f3770e3218ec360b195a174d97732b502
davemgj84 <davemgj@gmail.com>
2020-10-20 02:00:12 +0000

    create specs for user

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe Product, type: :model do
 
   describe 'Validations' do
 
-    it 'validates a new product' do
+    it 'Validates a new product' do
       new_category = Category.new(name: "Tests")
       new_product = Product.new(category: new_category, name: "hats", price_cents: 6000, quantity: 80)
       new_product.save
       expect(new_product).to be_valid
     end
 
-    it 'will not validate product without a name' do
+    it 'Will not validate product without a name' do
       new_category = Category.new(name: "Tests")
       new_product = Product.new(category: new_category, name: nil, price_cents: 6000, quantity: 80)
       new_product.save
@@ -19,7 +19,7 @@ RSpec.describe Product, type: :model do
       expect(new_product.errors.full_messages).to include("Name can't be blank")
     end
 
-    it 'will not validate product without a price' do
+    it 'Will not validate product without a price' do
       new_category = Category.new(name: "Tests")
       new_product = Product.new(category: new_category, name: "coats", price_cents: nil, quantity: 80)
       new_product.save
@@ -27,7 +27,7 @@ RSpec.describe Product, type: :model do
       expect(new_product.errors.full_messages).to include("Price can't be blank")
     end
 
-    it 'will not validate product without a quantity' do
+    it 'Will not validate product without a quantity' do
       new_category = Category.new(name: "Tests")
       new_product = Product.new(category: new_category, name: "shoes", price_cents: 6000, quantity: nil)
       new_product.save
@@ -35,7 +35,7 @@ RSpec.describe Product, type: :model do
       expect(new_product.errors.full_messages).to include("Quantity can't be blank")
     end
 
-    it 'will not validate product without a category' do
+    it 'Will not validate product without a category' do
       new_product = Product.new(category: nil, name: "pants", price_cents: 6000, quantity: 80)
       new_product.save
       expect(new_product).to_not be_valid
